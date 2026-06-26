5aa06da8b9165ca6e88b7bca13bba724cdf1f590
nayan-awasthi-iphtech <nayan.awasthi@iphtechnologies.com>
2026-06-26 12:03:23 +0530

    changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,14 +3,14 @@ import { cacheMiddle } from "../middleware/cacheMiddleware.js";
 
 const router = Router();
 
-router.route("/api/login").get(cacheMiddle, async (req, res, next) => {   
+router.route("/api/login").get(cacheMiddle, async (req, res, next) => {
     try {
         console.log("Data cached complete login successfull:",);
         res.json({ message: "Login successful", cachedData: req.cachedData });
     } catch (err) {
-        console.error(err);
+        console.error("Login error is coming here",err);
         res.status(500).send({ error: "Internal Server Error" });
     }
 });
 
-export default router;   
+export default router;
